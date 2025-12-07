9676fb7c694575992b8f68aa08e9bbcb0f82db97
Re issiki <yuriha765rikuto@gmail.com>
2025-12-07 23:40:40 +0900

    update

--- script.js
+++ script.js
@@ -368,15 +368,6 @@ function resetAllStatus(){
   saveData();
 }
 
-//全データ削除
-function resetAllData(){
-  if(!confirm("本当に全データをリセットしますか？")) return;
-  localStorage.removeItem("playerData");
-  localStorage.removeItem("quests");
-  localStorage.removeItem("homeGenerated");
-  location.reload(); // ページを再読み込みして初期状態に
-}
-
 // ===== 画面切替 =====
 function showManage(){
   document.getElementById("homeScreen").style.display="none";
